6e30b71eaee91cd16270c072b49475c041f8ae2d
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-07-24 13:32:31 +0200

    Delete useless lines

--- script/Main.js
+++ script/Main.js
@@ -4,8 +4,3 @@ function openWin() {
   myWindow = window.open("controlPanel.html", "", "width=600, height=600");
 }
 openWin();
-
-// chrome.system.display.geyInfo(function (displayInfo) {
-//   console.log(displayInfo);
-
-// });
